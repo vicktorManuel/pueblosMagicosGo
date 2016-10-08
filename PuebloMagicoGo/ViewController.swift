ebc4be40c23861d9efed3e401975dae49bf44282
victor <vicktor2555@gmail.com>
2016-10-07 22:30:34 -0500

    Initial Commit

---
+++ PuebloMagicoGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PuebloMagicoGo
+//
+//  Created by victor Franco on 07/10/16.
+//  Copyright © 2016 Grisostomos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
